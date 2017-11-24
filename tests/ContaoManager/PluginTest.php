8cebacf8cde45f2eca82c905c7c721c6eafb5bc4
Thomas Körner <t.koerner@heimrich-hannot.de>
2017-11-24 09:22:00 +0100

    setup phpunit, travis and coversall

---
+++ tests/ContaoManager/PluginTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace HeimrichHannot\ContaoNewsAlertBundle\Test\ContaoManager;
+
+use HeimrichHannot\ContaoNewsAlertBundle\ContaoManager\Plugin;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2017 Heimrich & Hannot GmbH
+ *
+ * @author  Thomas Körner <t.koerner@heimrich-hannot.de>
+ * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+ */
+
+class PluginTest extends TestCase
+{
+    public function testInstantiation()
+    {
+        static::assertInstanceOf(
+            \HeimrichHannot\ContaoNewsAlertBundle\ContaoManager\Plugin::class,
+            new Plugin
+        );
+    }
+}
